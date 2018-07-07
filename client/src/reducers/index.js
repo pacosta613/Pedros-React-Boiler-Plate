8b8bf91ae021542f8663e3f3a7a5f19fef77a4f3
Pedro Acosta <pedro.acosta613@gmail.com>
2018-07-07 18:06:05 -0400

    added babel file

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers }                      from 'redux';
 import { reducer as Form }                      from 'redux-form';
-
+import auth from './auth.js'
 
 const rootReducer = combineReducers({
+    auth: auth
 });
 
 export default rootReducer;
