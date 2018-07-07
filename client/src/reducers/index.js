b73c4dc837080a56c581136d9773260156e2025d
Pedro Acosta <pedro.acosta613@gmail.com>
2018-07-07 18:06:40 -0400

    remove file

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers }                      from 'redux';
 import { reducer as Form }                      from 'redux-form';
-import auth from './auth.js'
 
 const rootReducer = combineReducers({
-    auth: auth
 });
 
 export default rootReducer;
